601d1cfdc81f72c5d8de3f4e622e4f93f8dc1f39
Carolyn.Maynard <carolyn.maynard@rtx.com>
2026-05-12 08:25:32 -0700

    Remove using generic EWTS_ID

--- include/logger.h
+++ include/logger.h
@@ -1,7 +1,13 @@
 #ifndef PET_LOGGER_H
 #define PET_LOGGER_H
+
 #include "ewts/module_constants.h"
-#define EWTS_ID EWTS_ID_PET
 #include "ewts/logger.h"
 #include "ewts/log_levels.h"
+
+#define Log(level, ...) EwtsLogModule(EWTS_ID_PET, (level), __VA_ARGS__)
+#define LOG(level, ...) EwtsLogModule(EWTS_ID_PET, (level), __VA_ARGS__)
+#define GetLogLevel() EwtsGetLogLevelModule(EWTS_ID_PET)
+#define IsLoggingEnabled() EwtsIsLoggingEnabledModule(EWTS_ID_PET)
+
 #endif /* PET_LOGGER_H */
